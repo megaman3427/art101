b4098c34dd4a2150d245fe06ae59ed68b0184e93
megaman3427 <megaman3427@gmail.com>
2024-05-13 00:42:41 -0700

    Editng html

---
+++ lab9/js/lab.js
@@ -0,0 +1,30 @@
+
+/*
+   lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Revon Hollis
+   Date: 05-11-2024
+*/
+
+$("#challenge").append("<button id='button-challenge'>Challenge</button>");
+
+$("button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("Challenges");
+});
+
+$("#problems").append("<button id='button-problems'>Problems</button>");
+
+$("button-problems").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special");
+});
+
+$("#results").append("<button id='button-results'>Results</button>");
+
+$("button-results").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("special");
+});
