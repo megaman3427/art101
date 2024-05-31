622ca4f9d41f1cb00955c1249da7e8918426a99f
megaman3427 <megaman3427@gmail.com>
2024-05-31 01:01:35 -0700

    Editing HTML

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,23 +1,31 @@
-for (let i=1; i <=120; i++) {
-        
-            if ((i % 3   == 0) && (i % 5 == 0)) {
-                console.log ("FizzBuzz");
-            }
-        
+// script.js
 
-             else if (i % 3 == 0) {
-                console.log("Fizz");
-            }
-            
-            else if (i % 5 == 0) {
-                console.log("Buzz");
-            }
-            else if (i % 7 == 0) {
-                console.log("Boom");
-            }
-
-            else {
-                console.log(i);
-            }
+// Function to output text to the page
+function outputToPage(str) {
+    var outputEl = document.getElementById("output");
+    var newDiv = document.createElement("div");
+    newDiv.textContent = str;
+    outputEl.appendChild(newDiv);
 }
 
+// Function to run the FizzBuzzBoom logic
+function runFizzBuzzBoom() {
+    document.getElementById("output").innerHTML = ""; // Clear previous output
+
+    for (let i = 1; i <= 120; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            outputToPage("FizzBuzz");
+        } else if (i % 3 == 0) {
+            outputToPage("Fizz");
+        } else if (i % 5 == 0) {
+            outputToPage("Buzz");
+        } else if (i % 7 == 0) {
+            outputToPage("Boom");
+        } else {
+            outputToPage(i.toString());
+        }
+    }
+}
+
+// Add event listener to the button
+document.getElementById("runButton").addEventListener("click", runFizzBuzzBoom);
