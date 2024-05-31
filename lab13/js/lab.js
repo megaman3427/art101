7b9f718e7223ab30d282ac222fcaf1c465014376
megaman3427 <megaman3427@gmail.com>
2024-05-31 00:20:22 -0700

    Editing HTML

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -21,18 +21,3 @@ for (let i=1; i <=120; i++) {
             }
 }
 
-function reportError(str) {
-    outputEl.innerHTML = "<div class='error'>" + str + "</div>";
-
-}
-
-document.getElementById("submit").addEventListener("click", function() {
-    var max = document.getElementById("max").ariaValueMax;
-    console.log("max:", max)
-    if (! max) {
-        reportError("You must provide a maximum");
-    } else {
-        document.getElementById("output").innerHTML = ""; // Clear previous output
-        fizzBuzzBoom(parseInt(max), {3: "Fizz", 5: "Buzz"});
-    }
-});
